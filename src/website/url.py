1894275906ddbf88528117278c8e584acbebd1de
amirdotnetdev <amirkamyabi4@gmail.com>
2023-11-21 12:43:16 +0330

    upload via git

--- src/website/url.py
+++ src/website/url.py
@@ -4,5 +4,7 @@ from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path("",views.home,name='home'),
+    path("products/",views.products,name='products'),
+    path("logout/",views.logout,name='logout'),
 ]
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/website/views.py
+++ src/website/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth import logout
+from .models import *
 def home(request):
     return render(request,'home/home.html',)
+
+def products(request):
+    return render(request,'product/product.html')
+
+def logout(request):
+    logout(request)
+    return render(request,'home/home.html')
